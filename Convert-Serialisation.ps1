fa34ae2cabf083e2451adf940fbcd2d6bc24b628
Ibrahim Tayib <71063177+ibromangobyte@users.noreply.github.com>
2023-02-27 17:23:12 +1000

    Add files via upload

---
+++ Convert-Serialisation.ps1
@@ -0,0 +1,109 @@
+﻿<#
+
+.SYNOPSIS
+
+Automates the installation of Hyper-V. Works on either Pro or Home editions
+
+.DESCRIPTION
+
+This will force an automatic reboot and will pick up where it left off to
+complete the configuration.
+
+#>
+
+class SerialisationItem {
+    
+    [System.String]
+    $Name
+
+    [System.String]
+    $Path
+
+    [System.String]
+    $Database
+
+    [System.String]
+    $Scope = [System.String]::Empty
+
+    SerialisationItem(){}
+    
+    SerialisationItems([System.String] $name, [System.String] $path, [System.String] $database)
+    {
+        $this.Name = $name
+        $this.Path = $path
+        $this.Database = $database
+    }
+
+    SerialisationItems([System.String] $name, [System.String] $path, [System.String] $database, [System.String] $scope)
+    {
+        $this.Name = $name
+        $this.Path = $path
+        $this.Database = $database
+        $this.Scope = $scope
+    }
+
+    [System.Boolean] IsValidPath([System.String] $path)
+    {
+        if ($null -ne $path)
+        {
+            return ($path -match [Constants]::PathRegexPattern)
+        }
+
+        return false;
+    }
+}
+
+class SerialisationModule {
+        
+    [System.Object]
+    $Namespace
+    
+    [System.Collections.ArrayList]
+    $References = @()
+
+    [System.Collections.ArrayList]
+    $Items = @()
+    
+    SerialisationModule(){}
+    
+    SerialisationModule([System.String] $namespace, [System.Collections.ArrayList] $references, [System.Collections.ArrayList] $items)
+    {
+        $this.Namespace = $namespace
+        $this.References = $references
+        $this.Items = $items
+    }
+    
+    [System.Boolean] IsNotNullAndEmpty([System.Collections.ArrayList] $items)
+    {
+        if ($null -ne $items -and @($items).count -gt 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
+
+
+function Convert-Serialisation {
+    param (
+        [Parameter(Mandatory=$true)]
+        [System.String]
+        $Path
+    )
+
+}
+
+Begin
+{
+}
+
+Process 
+{
+}
+
+class Constants {
+    
+    static [System.String] 
+    $PathRegexPattern = "(^/[a-z0-9\s]+)(/[a-z0-9-\s]+)*([a-z0-9\s])$"
+}
